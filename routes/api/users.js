5bc67112b2a3cd0ee110c64ba73d19066436f0a9
yeliseyeva <nik29121997@gmail.com>
2022-01-07 22:23:32 +0200

    хеширование

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const { User } = require("../../model");
 const { joiSchema } = require("../../model/user");
 const { BadRequest, Conflict } = require("http-errors");
 
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 router.post("/signup", async (req, res, next) => {
@@ -13,13 +14,15 @@ router.post("/signup", async (req, res, next) => {
     if (error) {
       throw new BadRequest(error.message);
     }
-    const { password, email } = req.body;
+    const { email, password } = req.body;
     const user = await User.findOne({ email });
 
     if (user) {
-      throw new Conflict("User already exist");
+      throw new Conflict("Email in use");
     }
-    const newUser = await User.create(req.body);
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create({ email, password: hashPassword });
     res.status(201).json({
       user: {
         email: newUser.email,
